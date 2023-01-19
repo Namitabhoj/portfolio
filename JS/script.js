70bf573bce11198d5227ab34249ec0712002ba97
Namita Bhoj <namitabhoj@Namitas-MBP.cgocable.net>
2023-01-18 23:26:20 -0500

    added new about-page

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+
+const WebCifarIcon = document.querySelector("#webCifar-icon");
+const WebCifarEl = document.querySelector("#webCifar");
+const close = WebCifarEl.querySelector(".close");
+const youtubeLink = document.querySelector(".youtubeLink");
+
 hamburger.addEventListener('click', ()=>{
    //Links
     navLinks.classList.toggle("open");
@@ -14,3 +20,13 @@ hamburger.addEventListener('click', ()=>{
 }
 
 );
+
+WebCifarIcon.addEventListener("click", () => {
+  WebCifarEl.classList.add("active");
+});
+close.addEventListener("click", () => {
+  WebCifarEl.classList.remove("active");
+});
+
+youtubeLink.setAttribute("href", "https://youtu.be/_HTL0XSMcRQ");
+
